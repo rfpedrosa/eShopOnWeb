e0c186c4d5b7df3b79ff85da81d16a62cd9fe29f
Rui Pedrosa <rui.pedrosa@sursumcorda.com>
2020-12-22 10:08:11 +0000

    get integration tests working with ensure created and ensure deleted

---
+++ tests/IntegrationTests/Repositories/BaseEfRepoTestFixture.cs
@@ -0,0 +1,88 @@
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.eShopWeb.ApplicationCore.Entities;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.eShopWeb.Infrastructure.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.eShopWeb.IntegrationTests.Repositories
+{
+    // Based on https://github.com/ardalis/CleanArchitecture/blob/master/tests/CleanArchitecture.IntegrationTests/Data/BaseEfRepoTestFixture.cs
+    // and https://www.davepaquette.com/archive/2016/11/27/integration-testing-with-entity-framework-core-and-sql-server.aspx
+    public abstract class BaseEfRepoTestFixture : IDisposable
+    {
+        private static readonly IConfigurationRoot Configuration;
+
+        private readonly CatalogContext _catalogContext;
+
+        static BaseEfRepoTestFixture()
+        {
+            Configuration = BuildConfig();
+        }
+
+        protected BaseEfRepoTestFixture()
+        {
+            var dbOptions = CreateNewContextOptions(GetType().Name);
+
+            _catalogContext = new CatalogContext(dbOptions);
+
+            // EnsureCreated totally bypasses migrations and just creates the schema for you, you can't mix this with migrations.
+            // EnsureCreated is designed for testing or rapid prototyping where you are ok with dropping and re-creating the database each time.
+            // If you are using migrations and want to have them automatically applied on app start, then you can use context.Database.Migrate() instead.
+            _catalogContext.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            _catalogContext.Database.EnsureDeleted();
+        }
+
+        private static DbContextOptions<CatalogContext> CreateNewContextOptions(string testName)
+        {
+            var dbSuffix = $"{testName}_{Guid.NewGuid()}";
+
+            // Create a fresh service provider, and therefore a fresh
+            // Npgsql database instance.
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkNpgsql()
+                .BuildServiceProvider();
+
+            // Create a new options instance telling the context to use an
+            // Npgsql database and the new service provider.
+            var postgresConnectionString = string.Format(Configuration.GetConnectionString("IntegrationTestsDb"), dbSuffix);
+            var builder = new DbContextOptionsBuilder<CatalogContext>();
+            builder.UseNpgsql(postgresConnectionString)
+                .UseInternalServiceProvider(serviceProvider)
+                .UseSnakeCaseNamingConvention(); // https://www.npgsql.org/efcore/modeling/table-column-naming.html
+
+            return builder.Options;
+        }
+
+        private static IConfigurationRoot BuildConfig()
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+                .AddJsonFile("appsettings.json", true, reloadOnChange: false);
+
+            if (environmentName != null)
+            {
+                config.AddJsonFile($"appsettings.{environmentName}.json", true, reloadOnChange: false);
+            }
+
+            config.AddEnvironmentVariables();
+
+            return config.Build();
+        }
+
+        protected IAsyncRepository<T> GetRepository<T>() 
+            where T : BaseEntity, IAggregateRoot
+        {
+            return new EfRepository<T>(_catalogContext);
+        }
+    }
+}
--- tests/IntegrationTests/Repositories/BasketRepositoryTests/SetQuantities.cs
+++ tests/IntegrationTests/Repositories/BasketRepositoryTests/SetQuantities.cs
@@ -1,49 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
+﻿using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.ApplicationCore.Services;
-using Microsoft.eShopWeb.Infrastructure.Data;
 using Microsoft.eShopWeb.UnitTests.Builders;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
 namespace Microsoft.eShopWeb.IntegrationTests.Repositories.BasketRepositoryTests
 {
-    public class SetQuantities
+    public class SetQuantities : BaseEfRepoTestFixture
     {
-        private readonly CatalogContext _catalogContext;
         private readonly IAsyncRepository<Basket> _basketRepository;
         private readonly BasketBuilder BasketBuilder = new BasketBuilder();
 
         public SetQuantities()
         {
-            // Create a fresh service provider, and therefore a fresh
-            // Npgsql database instance.
-            var serviceProvider = new ServiceCollection()
-                .AddEntityFrameworkNpgsql()
-                .BuildServiceProvider();
-            
-            // Create a new options instance telling the context to use an
-            // Npgsql database and the new service provider.
-            // var postgresConnectionString = string.Format(Configuration.GetConnectionString("IntegrationTestsDb"), dbSuffix);
-            const string postgresConnectionString = "Host=localhost;Port=5433;Database=integration_tests;Username=postgres;Password=reallyStrongPwd123;Maximum Pool Size=1";
-            var builder = new DbContextOptionsBuilder<CatalogContext>();
-            builder.UseNpgsql(postgresConnectionString)
-                .UseInternalServiceProvider(serviceProvider)
-                .UseSnakeCaseNamingConvention(); // https://www.npgsql.org/efcore/modeling/table-column-naming.html
-
-            var dbOptions = builder.Options;
-            
-            _catalogContext = new CatalogContext(dbOptions);
-            
-            // EnsureCreated totally bypasses migrations and just creates the schema for you, you can't mix this with migrations.
-            // EnsureCreated is designed for testing or rapid prototyping where you are ok with dropping and re-creating the database each time.
-            // If you are using migrations and want to have them automatically applied on app start, then you can use context.Database.Migrate() instead.
-            _catalogContext.Database.EnsureCreated();
-            
-            _basketRepository = new EfRepository<Basket>(_catalogContext);
+            _basketRepository = GetRepository<Basket>();
         }
 
         [Fact]
@@ -52,7 +24,6 @@ namespace Microsoft.eShopWeb.IntegrationTests.Repositories.BasketRepositoryTests
             var basket = BasketBuilder.WithOneBasketItem();
             var basketService = new BasketService(_basketRepository, null);
             await _basketRepository.AddAsync(basket);
-            await _catalogContext.SaveChangesAsync();
 
             await basketService.SetQuantities(BasketBuilder.BasketId, new Dictionary<string, int>() { { BasketBuilder.BasketId.ToString(), 0 } });
 
